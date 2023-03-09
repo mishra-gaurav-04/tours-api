027950c7cc726ba2bc897df0cc363c781dd2121f
mishra-gaurav-04 <gaurav.info217@gmail.com>
2023-03-09 08:03:02 +0530

    added cookies

--- auth/authController.js
+++ auth/authController.js
@@ -12,6 +12,29 @@ const signToken = (id) => {
        });
 };
 
+const createSendToken = (user,statusCode,res) => {
+    try{const token = signToken(user._id);
+    const cookiesOptions = {
+        expires : new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN *24*60*60*1000),
+        httpOnly : true
+    };
+    if(process.env.NODE_ENV === 'production'){
+        cookiesOptions.secure = true;
+    }
+    user.password = undefined;
+    res.cookie('jwt',token,cookiesOptions);
+    res.status(statusCode).json({
+        status : 'Success',
+        token ,
+        data : {
+            user
+        }
+    });}
+    catch(err){
+        console.log(err);
+    }
+};
+
 exports.signup = catchAsync(async(req,res,next) => {
     // const newUser = await User.create({
     //     name : req.body.name,
@@ -20,15 +43,7 @@ exports.signup = catchAsync(async(req,res,next) => {
     //     passwordConfirm : req.body.passwordConfirm
     // });
     const newUser  = await User.create(req.body);
-   const token = signToken(newUser._id);
-    res.status(201).json({
-        status : 'Success',
-        message : 'User created successfully',
-        token,
-        data : {
-           user : newUser
-        }
-    })
+    createSendToken(newUser,201,res);
 });
 
 exports.login = async(req,res,next) => {
@@ -44,12 +59,7 @@ exports.login = async(req,res,next) => {
     if(!user || !(await user.correctPassword(password,user.password))){
         return next(new AppError('Invalid email or password',401));
     }
-    const token = signToken(user._id);
-    
-    res.status(200).json({
-        status : 'Success',
-        token
-    });
+    createSendToken(user,200,res);
 };
 
 exports.protect = catchAsync(async(req,res,next) => {
@@ -95,7 +105,6 @@ exports.forgotPassword = catchAsync(async(req,res,next) => {
     await user.save({validateBeforeSave : false});
     const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/reset-password/${resetToken}`;
     const message = `Click on the link to reset the password \n ${resetUrl}`;
-
     try{
         await sendEmail({
             email : user.email,
@@ -116,7 +125,6 @@ exports.forgotPassword = catchAsync(async(req,res,next) => {
     }
 
 });
-
 exports.resetPassword = catchAsync(async(req,res,next) => {
     const resetToken = req.params.token;
     const hashedToken = crypto.createHash('sha256').update(resetToken).digest('hex');
@@ -125,20 +133,13 @@ exports.resetPassword = catchAsync(async(req,res,next) => {
     if(!user){
         return next(new AppError('Invalid token',400));
     }
-
     user.password = req.body.password;
     user.passwordConfirm = req.body.passwordConfirm;
     user.passwordResetToken = undefined;
     user.passwordResetTokenExpiresIn = undefined;
     await user.save();
-
-    const token = await signToken(user._id);
-    res.status(200).json({
-        status : 'Success',
-        token
-    });
+    createSendToken(user,200,res);
 });
-
 exports.updatePassword = catchAsync(async(req,res,next) => {
     const user = await User.findById(req.user.id).select('+password');
     if(!(await user.correctPassword(req.body.passwordCurrent,user.password))){
@@ -147,10 +148,5 @@ exports.updatePassword = catchAsync(async(req,res,next) => {
     user.password = req.body.password;
     user.passwordConfirm = req.body.passwordConfirm;
     await user.save();
-
-    const token = await signToken(user._id);
-    res.status(200).json({
-        status : 'Success',
-        token
-    });
+    createSendToken(user,200,res);
 });
