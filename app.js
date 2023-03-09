c23a5b7b8684dc3e40c3f9040568385f2efc9808
mishra-gaurav-04 <gaurav.info217@gmail.com>
2023-03-09 08:46:37 +0530

    added prevent parameter pollution

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 dotenv.config();
@@ -41,6 +42,16 @@ app.use('/api',limiter);
 app.use(express.json({limit : '10kb'}));
 app.use(mongoSanitize());
 app.use(xss());
+app.use(hpp({
+    whitelist : [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAvg',
+        'price',
+        'difficulty',
+        'maxGroupSize'
+    ]
+}));
 app.use(express.static(path.join(__dirname,'./public')));
 // app.use((req,res,next) => {
 //     console.log(req.headers);
