b835d0613e5233745b99e00d20769dcef5855de2
mishra-gaurav-04 <gaurav.info217@gmail.com>
2023-03-12 10:33:24 +0530

    added refactoring

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const fs = require('fs');
 const Tours = require('../../models/Tours');
+const Users = require('../../models/Users');
+const Reviews = require('../../models/Reviews');
 
 dotenv.config({path : '/home/gaurav/Development/Web-D/Backend/tours/.env'});
 
@@ -15,11 +17,15 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // reading json file
 const tours = JSON.parse(fs.readFileSync('/home/gaurav/Development/Web-D/Backend/tours/dev-data/data/tours.json','utf-8'));
+const users = JSON.parse(fs.readFileSync('/home/gaurav/Development/Web-D/Backend/tours/dev-data/data/users.json','utf-8'));
+const reviews = JSON.parse(fs.readFileSync('/home/gaurav/Development/Web-D/Backend/tours/dev-data/data/reviews.json','utf-8'));
 
 // loading data to DB
 const importData = async() => {
     try{
         await Tours.create(tours);
+        await Users.create(users,{validateBeforeSave:false});
+        await Reviews.create(reviews);
         console.log('Data loaded successsfully to database');
         process.exit();
     }
@@ -32,6 +38,8 @@ const importData = async() => {
 const deleteData = async() => {
     try{
         await Tours.deleteMany();
+        await Users.deleteMany();
+        await Reviews.deleteMany();
         console.log('Data deleted Successfully');
         process.exit();
     }
